3307f1b8966f703ab37726a9e79c3c6e233165de
Faresis <nikignatiev@gmail.com>
2016-06-19 13:07:06 +0300

    Logging added to the Bookshelf Service.

--- com.packtpub.felix.bookshelf-service/src/main/java/com/packtpub/felix/bookshelf/service/impl/BookshelfServiceImpl.java
+++ com.packtpub.felix.bookshelf-service/src/main/java/com/packtpub/felix/bookshelf/service/impl/BookshelfServiceImpl.java
@@ -1,6 +1,7 @@
 package com.packtpub.felix.bookshelf.service.impl;
 
 import com.packtpub.felix.bookshelf.inventory.api.*;
+import com.packtpub.felix.bookshelf.log.api.BookshelfLogHelper;
 import com.packtpub.felix.bookshelf.service.api.BookshelfService;
 import com.packtpub.felix.bookshelf.service.api.InvalidCredentialsException;
 
@@ -15,10 +16,13 @@ public class BookshelfServiceImpl implements BookshelfService {
 
     private BookInventory inventory;
 
+    private BookshelfLogHelper logger;
+
     public BookshelfServiceImpl() {
     }
 
     public String login(String userName, char[] password) throws InvalidCredentialsException {
+        getLogger().debug("Logging in with {0}, {1}", new Object[] { userName, password });
         if ("admin".equals(userName) &&
                 Arrays.equals(password, "admin".toCharArray())) {
             this.sessionId = Long.toString(System.currentTimeMillis());
@@ -28,25 +32,31 @@ public class BookshelfServiceImpl implements BookshelfService {
     }
 
     public void logout(String sessionId) {
+        getLogger().debug("Logging out with {0}", sessionId);
         checkSession(sessionId);
         this.sessionId = null;
     }
 
     public boolean sessionIsValid(String sessionId) {
+        getLogger().debug("Checking session {0}", sessionId);
         return this.sessionId != null && this.sessionId.equals(sessionId);
     }
 
     public Book getBook(String sessionId, String isbn) throws BookNotFoundException {
+        getLogger().debug("Getting book for {0}, by isbn: {1}", sessionId, isbn);
         checkSession(sessionId);
         return inventory.loadBook(isbn);
     }
 
     public MutableBook getBookForEdit(String sessionId, String isbn) throws BookNotFoundException {
+        getLogger().debug("Getting book for edit for {0}, by isbn: {1}", sessionId, isbn);
         checkSession(sessionId);
         return inventory.loadBookForEdit(isbn);
     }
 
     public void addBook(String session, String isbn, String title, String author, String category, int rating) throws BookAlreadyExistsException, InvalidBookException {
+        getLogger().debug("Adding book for {0}, with isbn {1}, title {2}, author {3}, category {4}, rating {5}",
+                session, isbn, title, author, category, rating);
         checkSession(session);
 
         final MutableBook book = inventory.createBook(isbn);
@@ -58,6 +68,7 @@ public class BookshelfServiceImpl implements BookshelfService {
     }
 
     public void modifyBookCategory(String session, String isbn, String category) throws BookNotFoundException, InvalidBookException {
+        getLogger().debug("Modifying book category for {0}, with isbn {1}, category {2}", session, isbn, category);
         checkSession(session);
 
         final MutableBook book = inventory.loadBookForEdit(isbn);
@@ -66,6 +77,7 @@ public class BookshelfServiceImpl implements BookshelfService {
     }
 
     public void modifyBookRating(String session, String isbn, int rating) throws BookNotFoundException, InvalidBookException {
+        getLogger().debug("Updating book rating for {0}, with isbn {1}, rating {2}", session, isbn, rating);
         checkSession(session);
 
         final MutableBook book = inventory.loadBookForEdit(isbn);
@@ -74,17 +86,20 @@ public class BookshelfServiceImpl implements BookshelfService {
     }
 
     public Set<String> getCategories(String session) {
+        getLogger().debug("Getting categories for {0}", session);
         checkSession(sessionId);
         return inventory.getCategories();
     }
 
     public void removeBook(String session, String isbn) throws BookNotFoundException {
+        getLogger().debug("Removing book for {0}, with isbn {1}", session, isbn);
         checkSession(session);
 
         inventory.removeBook(isbn);
     }
 
     public Set<String> searchBooksByCategory(String session, String categoryLike) {
+        getLogger().debug("Searching books by category for {0}, with {1}", session, categoryLike);
         checkSession(session);
         Map<BookInventory.SearchCriteria, String> criteria = new HashMap<BookInventory.SearchCriteria, String>();
         criteria.put(BookInventory.SearchCriteria.CATEGORY_LIKE, categoryLike);
@@ -92,6 +107,7 @@ public class BookshelfServiceImpl implements BookshelfService {
     }
 
     public Set<String> searchBooksByAuthor(String session, String authorLike) {
+        getLogger().debug("Searching books by author for {0}, with {1}", session, authorLike);
         checkSession(session);
         Map<BookInventory.SearchCriteria, String> criteria = new HashMap<BookInventory.SearchCriteria, String>();
         criteria.put(BookInventory.SearchCriteria.AUTHOR_LIKE, authorLike);
@@ -99,6 +115,7 @@ public class BookshelfServiceImpl implements BookshelfService {
     }
 
     public Set<String> searchBooksByTitle(String session, String titleLike) {
+        getLogger().debug("Searching books by title for {0}, with {1}", session, titleLike);
         checkSession(session);
         Map<BookInventory.SearchCriteria, String> criteria = new HashMap<BookInventory.SearchCriteria, String>();
         criteria.put(BookInventory.SearchCriteria.TITLE_LIKE, titleLike);
@@ -106,6 +123,7 @@ public class BookshelfServiceImpl implements BookshelfService {
     }
 
     public Set<String> searchBooksByRating(String session, int ratingLower, int ratingUpper) {
+        getLogger().debug("Searching books by rating for {0}, from {1} to {2}", session, ratingLower, ratingUpper);
         checkSession(session);
         Map<BookInventory.SearchCriteria, String> criteria = new HashMap<BookInventory.SearchCriteria, String>();
         criteria.put(BookInventory.SearchCriteria.GRADE_GT, Integer.toString(ratingLower));
@@ -114,8 +132,13 @@ public class BookshelfServiceImpl implements BookshelfService {
     }
 
     protected void checkSession(String sessionId) {
+        getLogger().debug("Checking session {0}", sessionId);
         if (!sessionIsValid(sessionId)) {
             throw new SessionNotValidRuntimeException(sessionId);
         }
+    }
+
+    private BookshelfLogHelper getLogger() {
+        return logger;
     }
 }
